c22f4dc0881068208545763dfdee52b36b3d0169
Christopher WInstanley <chris.winstanley@bbc.co.uk>
2024-02-16 11:45:38 +0000

    Initial Commit

---
+++ PictureFrame/PictureFrameApp.swift
@@ -0,0 +1,17 @@
+//
+//  PictureFrameApp.swift
+//  PictureFrame
+//
+//  Created by Chris Winstanley on 16/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct PictureFrameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
